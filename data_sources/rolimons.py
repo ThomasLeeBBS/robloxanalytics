b185d38982bb9d336d4dced27e0675f98ba0b108
Tomlee03bu <thomasbizarro@gmail.com>
2025-06-27 10:39:13 -0700

    fix: Add --no-sandbox option for headless Chrome as root

--- data_sources/rolimons.py
+++ data_sources/rolimons.py
@@ -61,6 +61,8 @@ class RolimonsSource:
         options = Options()
         options.add_argument("--disable-gpu")
         options.add_argument("--window-size=1920,1080")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
         if self.config['selenium']['headless']:
             options.add_argument("--headless")
         
